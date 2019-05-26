9386e4d1b5a37abbb3dcea00cc9d0504c15bf3ce
nikmakan-olx <nikhil.makan@olx.com>
2019-05-27 01:18:39 +0200

    organise header meta slightly

--- resources/views/pwa.blade.php
+++ resources/views/pwa.blade.php
@@ -1,25 +1,28 @@
 <!doctype html>
 <html>
 <head>
+    <title>Rounds</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Rounds</title>
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="msapplication-TileImage" content="img/icon.png">
     <link rel="apple-touch-icon" sizes="180x180" href="img/icon.png">
     <link rel="icon" type="image/svg" sizes="192x192"  href="img/icon.png">
     <link rel="icon" type="image/svg" sizes="250x250" href="img/icon.png">
-    <meta name="msapplication-TileImage" content="img/icon.png">
-    <link href="https://fonts.googleapis.com/css?family=Heebo:400,500,700,900" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Heebo:400,500,700,900">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.js'></script>
-    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.css' rel='stylesheet' />
 
-    <link href="//cdn.muicss.com/mui-0.9.41/css/mui.min.css" rel="stylesheet" type="text/css" />
+    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.js'></script>
+    <link rel="stylesheet" href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.css' />
+
+    <link rel="stylesheet" href="//cdn.muicss.com/mui-0.9.41/css/mui.min.css" />
     <script src="//cdn.muicss.com/mui-0.9.41/js/mui.min.js"></script>
     <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
     <script>
