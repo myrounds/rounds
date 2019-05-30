eb1741771cf79c1b7925e370908380a0df49b062
nikmakan-olx <nikhil.makan@olx.com>
2019-05-30 11:33:04 +0200

    basic members section (read-only for now, will do write after redesign)

--- resources/js/views/Members.vue
+++ resources/js/views/Members.vue
@@ -1,26 +1,95 @@
 <template>
-    <div>
+    <div class="members">
         <div class="mui--appbar-height"></div>
         <div class="mui-container-fluid">
 
-            <br>
+            <button class="mui-btn mui-btn--primary mui-btn--fab" style="position: fixed; bottom: 30px; right: 30px" @click="createMember">+</button>
 
             <div class="mui-panel">
-
-                LIST & EDIT MEMBERS
-
+                <div class="row header">
+                    <div class="col">Name</div>
+                    <div class="col">Email</div>
+                    <div class="col">Phone</div>
+                    <div class="col">License Plate</div>
+                </div>
+                <div class="row" v-for="member in members" @click="selectMember">
+                    <div class="col" :id="member.id">{{member.name}}</div>
+                    <div class="col" :id="member.id">{{member.email}}</div>
+                    <div class="col" :id="member.id">{{member.phone}}</div>
+                    <div class="col" :id="member.id">{{member.license_plate}}</div>
+                </div>
             </div>
 
         </div>
+
+        <modal v-if="showManageMember" @close="showManageMember = false">
+            <h3 slot="header">
+                {{selected.name || 'New Member'}}
+            </h3>
+            <div slot="body">
+
+                <div class="mui-textfield mui-textfield--float-label">
+                    <input type="text" :value="selected.name">
+                    <label>Name</label>
+                </div>
+
+                <div class="mui-textfield mui-textfield--float-label">
+                    <input type="email" :value="selected.email">
+                    <label>Email</label>
+                </div>
+
+                <div class="mui-textfield mui-textfield--float-label">
+                    <input type="text" :value="selected.phone">
+                    <label>Phone</label>
+                </div>
+
+                <div class="mui-textfield mui-textfield--float-label">
+                    <input type="text" :value="selected.license_plate">
+                    <label>License Plate</label>
+                </div>
+
+                <div class="mui-textfield">
+                    <textarea>{{selected.notes}}</textarea>
+                    <label>Notes</label>
+                </div>
+
+                <button class="mui-btn mui-btn--raised">Save</button>
+
+            </div>
+        </modal>
+
     </div>
 </template>
 
 <script>
+    import Storage from '../helpers/storage';
+    import Modal from '../components/Modal';
     export default {
+        components: {Modal},
         data() {
             return {
                 loading: false,
+                members: [],
+                selected: {},
+                showManageMember: false
             };
+        },
+        created() {
+            this.members = Storage.get('members');
+            this.error = this.users = null;
+            this.loading = true;
+        },
+        methods: {
+            selectMember(event) {
+                const id = event.target.id;
+
+                this.selected = this.members.find(member => parseInt(member.id) === parseInt(id));
+                this.showManageMember = true;
+            },
+            createMember() {
+                this.selected = {};
+                this.showManageMember = true;
+            }
         }
     }
 </script>
